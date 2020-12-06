9c6c639e77f316a5109ce79f75fdca39e20ae92a
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-06 22:23:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 document.querySelector('main#main').remove();
-let newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory');
+let newHeader = document.createElement('h1').setAttribute('id', 'victory');
 document.body.appendChild(newHeader);
