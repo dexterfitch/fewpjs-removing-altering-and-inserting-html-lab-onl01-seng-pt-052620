85800bffd4b17615c5da56bc5ef0278dfa46e513
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-06 22:25:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelector('main#main').remove();
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id', 'victory');
-document.body.prependChild(newHeader);
+document.body.appendChild(newHeader);
 newHeader.innerHTML = 'YOUR-NAME is the champion';
