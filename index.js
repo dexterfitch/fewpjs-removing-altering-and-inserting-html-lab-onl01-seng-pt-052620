cb60df519b95631a80a973bee6990407f6d6665f
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-06 22:25:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ document.querySelector('main#main').remove();
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id', 'victory');
 document.body.appendChild(newHeader);
-newHeader.innerHTML('<p>BIRDS</p>');
+newHeader.innerHTML = '<p>BIRDS</p>';
